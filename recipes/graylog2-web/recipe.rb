14515e8dd49126e744e4be3568ed0acb547bae92
Bernd Ahlers <bernd@torch.sh>
2014-05-16 18:26:46 +0200

    Start recipe for graylog2-web.

---
+++ recipes/graylog2-web/recipe.rb
@@ -0,0 +1,74 @@
+class Graylog2Web < FPM::Cookery::Recipe
+  description 'Graylog2 web'
+
+  name     'graylog2-web'
+  version  '0.20.1'
+  homepage 'http://graylog2.org/'
+  arch     'all'
+
+  source "https://github.com/Graylog2/graylog2-web-interface/releases/download/#{version}/graylog2-web-interface-#{version}.tgz"
+  sha256 '96b701318ad6e4fa6ca52419eba8da9f3c26f705d767f709bea86c902e40fc07'
+
+  maintainer 'TORCH GmbH <hello@torch.sh>'
+  vendor     'torch'
+  license    'GPLv3'
+
+  config_files '/etc/graylog2/web/application.conf',
+               '/etc/graylog2/web/graylog2-web-interface.conf',
+               '/etc/graylog2/web/logback.xml',
+               '/etc/graylog2/web/play.plugins'
+
+  platforms [:ubuntu] do
+    section 'net'
+    depends 'openjdk-7-jre-headless'
+
+    config_files '/etc/init/graylog2-web.conf',
+                 '/etc/default/graylog2-web'
+
+    post_install 'files/ubuntu/post-install'
+    post_uninstall 'files/ubuntu/post-uninstall'
+  end
+
+  platforms [:centos] do
+    depends 'java-1.7.0-openjdk'
+
+#    config_files '/etc/init.d/graylog2-web',
+#                 '/etc/sysconfig/graylog2-web'
+
+#    post_install 'files/centos/post-install'
+#    pre_uninstall 'files/centos/pre-uninstall'
+  end
+
+  def build
+  end
+
+  def install
+    case FPM::Cookery::Facts.platform
+    when :ubuntu
+      etc('init').install osfile('upstart.conf'), 'graylog2-web.conf'
+      etc('default').install osfile('default'), 'graylog2-web'
+#    when :centos
+#      etc('init.d').install osfile('init.d'), 'graylog2-web'
+#      etc('init.d/graylog2-web').chmod(0755)
+#      etc('sysconfig').install osfile('sysconfig'), 'graylog2-web'
+    end
+
+    etc('graylog2/web').install Dir['conf/*']
+    etc('graylog2/web').install file('logback.xml')
+
+    share('graylog2-web').install %w(README.md lib share)
+    share('graylog2-web/bin').install 'bin/graylog2-web-interface'
+
+    safesystem "ln -sf /etc/graylog2/web #{share("graylog2-web/conf")}"
+  end
+
+  private
+
+  def osfile(name)
+    workdir(File.join('files', FPM::Cookery::Facts.platform.to_s, name))
+  end
+
+  def file(name)
+    workdir(File.join('files', name))
+  end
+end
