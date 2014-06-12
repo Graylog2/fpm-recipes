9872527d74ce17d099f103ceda2fd6d559c5c08e
Bernd Ahlers <bernd@torch.sh>
2014-06-12 14:38:06 +0200

    Start graylog2-radio recipe for ubuntu, debian and centos.

---
+++ recipes/graylog2-radio/files/centos/init.d
@@ -0,0 +1,79 @@
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          graylog2-radio
+# Required-Start:    $network $named $remote_fs $syslog
+# Required-Stop:     $network $named $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Graylog2 Radio
+# Description:       Graylog2 Radio - Search your logs, create charts, send reports and be alerted when something happens.
+### END INIT INFO
+
+# Author: Lee Briggs <lee@leebriggs.co.uk>
+# Contributor: Sandro Roth <sandro.roth@gmail.com>
+# Contributor: Bernd Ahlers <bernd@torch.sh>
+
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DESC="Graylog2 Radio"
+NAME=graylog2-radio
+JAR_FILE=/usr/share/graylog2-radio/graylog2-radio.jar
+JAVA=/usr/bin/java
+PID_FILE=/var/run/graylog2/$NAME.pid
+JAVA_ARGS="-jar -Dlog4j.configuration=file:///etc/graylog2/radio/log4j.xml $JAR_FILE -p $PID_FILE -f /etc/graylog2.conf"
+SCRIPTNAME=/etc/init.d/$NAME
+LOCKFILE=/var/lock/subsys/$NAME
+GRAYLOG2_RADIO_USER=graylog2-radio
+GRAYLOG2_RADIO_JAVA_OPTS="-Xms1024m -Xmx1024m"
+# Pull in sysconfig settings
+[ -f /etc/sysconfig/${NAME} ] && . /etc/sysconfig/${NAME}
+
+# Exit if the package is not installed
+[ -e "$JAR_FILE" ] || exit 0
+[ -x "$JAVA" ] || exit 0
+
+start() {
+    echo -n $"Starting ${NAME}: "
+    daemon --check $JAVA --pidfile=${PID_FILE} --user=${GRAYLOG2_RADIO_USER} \
+        $JAVA $GRAYLOG2_RADIO_JAVA_OPTS $JAVA_ARGS $GRAYLOG2_RADIO_ARGS &
+    RETVAL=$?
+    echo
+    sleep 2
+    [ $RETVAL = 0 ] && touch ${LOCKFILE}
+    return $RETVAL
+}
+
+stop() {
+    echo -n $"Stopping ${NAME}: "
+    killproc -p ${PID_FILE} -d 10 $JAVA
+    RETVAL=$?
+    echo
+    [ $RETVAL = 0 ] && rm -f ${PID_FILE} && rm -f ${LOCKFILE}
+    return $RETVAL
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    status)
+        status -p ${PID_FILE} $NAME
+        RETVAL=$?
+        ;;
+    restart|force-reload)
+        stop
+        start
+        ;;
+    *)
+        N=/etc/init.d/${NAME}
+        echo "Usage: $N {start|stop|status|restart|force-reload}" >&2
+        RETVAL=2
+        ;;
+esac
+
+exit $RETVAL
---
+++ recipes/graylog2-radio/recipe.rb
@@ -0,0 +1,84 @@
+class Graylog2Radio < FPM::Cookery::Recipe
+  description 'Graylog2 radio'
+
+  name     'graylog2-radio'
+  version  '0.20.2'
+  revision 1
+  homepage 'http://graylog2.org/'
+  arch     'all'
+
+  source "https://github.com/Graylog2/graylog2-server/releases/download/#{version}/graylog2-radio-#{version}.tgz"
+  sha256 'bca944e40ba212f2d87f15dfcf915c527b5461dd334bd537bfbcc02a53bb5cec'
+
+  maintainer 'TORCH GmbH <hello@torch.sh>'
+  vendor     'torch'
+  license    'GPLv3'
+
+  config_files '/etc/graylog2-radio.conf',
+               '/etc/graylog2/radio/log4j.xml'
+
+  platforms [:ubuntu, :debian] do
+    section 'net'
+    depends 'openjdk-7-jre-headless', 'uuid-runtime'
+
+    config_files '/etc/default/graylog2-radio'
+
+    post_install "files/#{platform}/post-install"
+    post_uninstall "files/#{platform}/post-uninstall"
+  end
+
+  platforms [:ubuntu] do
+    config_files '/etc/init/graylog2-radio.conf'
+  end
+
+  platforms [:debian] do
+    config_files '/etc/init.d/graylog2-radio',
+                 '/etc/logrotate.d/graylog2-radio'
+  end
+
+  platforms [:centos] do
+    depends 'java-1.7.0-openjdk', 'util-linux-ng'
+
+    config_files '/etc/init.d/graylog2-radio',
+                 '/etc/sysconfig/graylog2-radio'
+
+    post_install 'files/centos/post-install'
+    pre_uninstall 'files/centos/pre-uninstall'
+  end
+
+  def build
+    patch(workdir('patches/graylog2-radio.conf.patch'))
+  end
+
+  def install
+    etc.install 'graylog2-radio.conf.example', 'graylog2-radio.conf'
+    etc('graylog2/radio').install file('log4j.xml')
+
+    case FPM::Cookery::Facts.platform
+    when :ubuntu
+      etc('init').install osfile('upstart.conf'), 'graylog2-radio.conf'
+      etc('default').install osfile('default'), 'graylog2-radio'
+    when :debian
+      etc('init.d').install osfile('init.d'), 'graylog2-radio'
+      etc('init.d/graylog2-radio').chmod(0755)
+      etc('default').install osfile('default'), 'graylog2-radio'
+      etc('logrotate.d').install osfile('logrotate'), 'graylog2-radio'
+    when :centos
+      etc('init.d').install osfile('init.d'), 'graylog2-radio'
+      etc('init.d/graylog2-radio').chmod(0755)
+      etc('sysconfig').install osfile('sysconfig'), 'graylog2-radio'
+    end
+
+    share('graylog2-radio').install 'graylog2-radio.jar'
+  end
+
+  private
+
+  def osfile(name)
+    workdir(File.join('files', FPM::Cookery::Facts.platform.to_s, name))
+  end
+
+  def file(name)
+    workdir(File.join('files', name))
+  end
+end
