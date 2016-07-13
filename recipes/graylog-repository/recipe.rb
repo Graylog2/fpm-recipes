40e42caab6a4f3c50a03d6854a07f76f797d87b7
Bernd Ahlers <bernd@graylog.com>
2016-07-13 17:05:41 +0200

    Add 2.0 to replaces in repository package

--- recipes/graylog-repository/recipe.rb
+++ recipes/graylog-repository/recipe.rb
@@ -9,7 +9,7 @@ class GraylogRepository < FPM::Cookery::Recipe
 
   name       "graylog-#{VERSION}-repository"
   version    '1'
-  revision   1
+  revision   2
   source     '', :with => :noop
   arch       'all'
   homepage   data.homepage
@@ -19,7 +19,7 @@ class GraylogRepository < FPM::Cookery::Recipe
 
   replaces %w(0.92 0.91 0.90).map {|v|
     "graylog2-#{v}-repository-#{os}#{osrel}"
-  }.concat(%w(1.0 1.1 1.2 1.3).map {|v|
+  }.concat(%w(1.0 1.1 1.2 1.3 2.0).map {|v|
     "graylog-#{v}-repository-#{os}#{osrel}"
   })
 
