5c0547427d94f8038efb3b987c56805c3cd42f4c
Donald Morton <donald.morton@graylog.com>
2020-04-24 11:57:35 -0500

    Bump version to 3.3.0-beta.1

--- recipes/graylog-repository/recipe.rb
+++ recipes/graylog-repository/recipe.rb
@@ -21,7 +21,7 @@ class GraylogRepository < FPM::Cookery::Recipe
     "graylog2-#{v}-repository-#{os}#{osrel}"
   }.concat(%w(1.0 1.1 1.2 1.3).map {|v|
     "graylog-#{v}-repository-#{os}#{osrel}"
-  }).concat(%w(2.0 2.1 2.2 2.3 2.4 2.5 3.0 3.1).map {|v|
+  }).concat(%w(2.0 2.1 2.2 2.3 2.4 2.5 3.0 3.1 3.2).map {|v|
     "graylog-#{v}-repository"
   })
 
