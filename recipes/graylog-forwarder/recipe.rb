b448a2f6ae6c4ef41a49a2449fe5b26b9b44b272
Donald Morton <donald.morton@graylog.com>
2020-11-03 14:47:01 -0600

    Add recommendations from PR review.

--- recipes/graylog-forwarder/recipe.rb
+++ recipes/graylog-forwarder/recipe.rb
@@ -37,13 +37,15 @@ class GraylogForwarder < FPM::Cookery::Recipe
 
   def install
     etc('graylog/forwarder').install file('forwarder.conf'), 'forwarder.conf'
+    etc('graylog/forwarder').install Dir['config/jvm.options']
+    
     share('graylog-forwarder').install 'graylog-cloud-forwarder.jar'
 
     share('graylog-forwarder').install 'bin'
     share('graylog-forwarder/bin').install Dir['bin/*']
 
     share('graylog-forwarder').install 'config'
-    share('graylog-forwarder/config').install Dir['config/jvm.options']
+
 
     share('graylog-forwarder').install 'plugin'
     share('graylog-forwarder/plugin').install Dir['plugin/*.jar']
