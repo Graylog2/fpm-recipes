eb75b26f11d79a4e27b8cd8170cfafce46883f0f
Donald Morton <donald.morton@graylog.com>
2020-11-11 14:37:25 -0600

    Initial draft of graylog-enterprise package

---
+++ recipes/graylog-enterprise/files/graylog-server.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# For Debian/Ubuntu based systems.
+if [ -f "/etc/default/graylog-server" ]; then
+    . "/etc/default/graylog-server"
+fi
+
+# For RedHat/Fedora based systems.
+if [ -f "/etc/sysconfig/graylog-server" ]; then
+    . "/etc/sysconfig/graylog-server"
+fi
+
+if [ -f "/usr/share/graylog-server/installation-source.sh" ]; then
+    . "/usr/share/graylog-server/installation-source.sh"
+fi
+
+# Java versions > 8 don't support UseParNewGC
+if ${JAVA:=/usr/bin/java} -XX:+PrintFlagsFinal 2>&1 | grep -q UseParNewGC; then
+	GRAYLOG_SERVER_JAVA_OPTS="$GRAYLOG_SERVER_JAVA_OPTS -XX:+UseParNewGC"
+fi
+
+$GRAYLOG_COMMAND_WRAPPER ${JAVA:=/usr/bin/java} $GRAYLOG_SERVER_JAVA_OPTS \
+    -jar -Dlog4j.configurationFile=file:///etc/graylog/server/log4j2.xml \
+    -Djava.library.path=/usr/share/graylog-server/lib/sigar \
+    -Dgraylog2.installation_source=${GRAYLOG_INSTALLATION_SOURCE:=unknown} \
+    /usr/share/graylog-server/graylog.jar server -f /etc/graylog/server/server.conf -np \
+    $GRAYLOG_SERVER_ARGS
---
+++ recipes/graylog-enterprise/recipe.rb
@@ -0,0 +1,78 @@
+require_relative '../tools'
+
+class GraylogServer < FPM::Cookery::Recipe
+  include Tools
+
+  description 'Graylog Enterprise Server'
+
+  name     'graylog-enterprise'
+  version  data.version
+  revision data.revision
+  homepage data.homepage
+  arch     'all'
+
+  source data.source
+  sha256 data.sha256
+
+  maintainer data.maintainer
+  vendor     data.vendor
+  license    data.license
+
+  replaces 'graylog-server'
+  conflicts 'graylog2-web', 'graylog-web', 'graylog', 'graylog2-server'
+
+  config_files '/etc/graylog/server/server.conf',
+               '/etc/graylog/server/log4j2.xml',
+               '/etc/init.d/graylog-server'
+
+  targets :deb do
+    section 'net'
+    config_files '/etc/default/graylog-server',
+                 '/etc/init/graylog-server.conf',
+                 '/etc/logrotate.d/graylog-server'
+    depends 'uuid-runtime'
+  end
+
+  targets :rpm do
+    config_files '/etc/sysconfig/graylog-server'
+  end
+
+  pre_install    'files/pre-install'
+  post_install   'files/post-install'
+  pre_uninstall  'files/pre-uninstall'
+  post_uninstall 'files/post-uninstall'
+
+  def build
+    patch(workdir('patches/graylog-server.conf.patch'))
+  end
+
+  def install
+    case target
+    when :deb
+      etc('default').install file('environment'), 'graylog-server'
+      etc('init').install file('deb/upstart.conf'), 'graylog-server.conf'
+      etc('logrotate.d').install file('deb/logrotate'), 'graylog-server'
+    when :rpm
+      etc('sysconfig').install file('environment'), 'graylog-server'
+    end
+
+    etc('graylog/server').install 'graylog.conf.example', 'server.conf'
+    etc('graylog/server').install file('log4j2.xml')
+
+    etc('init.d').install file("#{target}/init.d"), 'graylog-server'
+    etc('init.d/graylog-server').chmod(0755)
+
+    lib('systemd/system').install file('systemd.service'), 'graylog-server.service'
+
+    share('graylog-server/bin').install file('graylog-server.sh'), 'graylog-server'
+    share('graylog-server/bin/graylog-server').chmod(0755)
+    share('graylog-server').install file("installation-source.#{target}.sh"), 'installation-source.sh'
+
+    share('graylog-server').install 'graylog.jar'
+    share('graylog-server').install 'lib'
+    share('graylog-server').install 'plugin'
+
+    # Remove unused sigar libs.
+    sigar_cleanup(share('graylog-server/lib/sigar'))
+  end
+end
