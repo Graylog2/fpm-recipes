517a938738780c04dd9b0264e105846ecf2fa384
Donald Morton <donald.morton@graylog.com>
2020-11-19 10:14:27 -0600

    Add jenkins pipeline and package update script.

---
+++ scripts/jenkins.groovy
@@ -0,0 +1,34 @@
+pipeline
+{
+   agent { label 'packages' }
+
+   options
+   {
+      buildDiscarder logRotator(artifactDaysToKeepStr: '90', artifactNumToKeepStr: '100', daysToKeepStr: '90', numToKeepStr: '100')
+      skipDefaultCheckout(true)
+      timestamps()
+   }
+
+   parameters
+   {
+     string(name: 'VERSION', description: 'Version of Graylog to build (3.2, 3.3, etc).')
+     gitParameter branchFilter: 'origin/(.*)', defaultValue: 'master', selectedValue: 'DEFAULT', name: 'BRANCH', type: 'PT_BRANCH', sortMode: 'DESCENDING_SMART', description: 'The branch of fpm-recipes that should be checked out (3.2, 3.3, master, etc).'
+   }
+
+   stages
+   {
+      stage('Build Packages')
+      {
+         steps
+         {
+            script
+            {
+              checkout([$class: 'GitSCM', branches: [[name: "refs/heads/${params.BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-app', url: 'https://github.com/Graylog2/fpm-recipes.git']]])
+            }
+
+            sh "gl2-build-pkg debian ${params.VERSION}"
+            sh "gl2-build-pkg el ${params.VERSION}"
+         }
+      }
+   }
+}
