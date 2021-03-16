6f3f543e8f2f34a2861b315a2c7cfed18109d76d
Donald Morton <donald.morton@graylog.com>
2021-03-16 14:30:44 -0500

    Update illuminate package to install csv files.

--- recipes/graylog-illuminate/recipe.rb
+++ recipes/graylog-illuminate/recipe.rb
@@ -16,16 +16,35 @@ class GraylogIlluminate < FPM::Cookery::Recipe
   vendor     'graylog'
   license    data.license
 
-  #config_files '/etc/graylog/sidecar/sidecar.yml'
+  config_files   Dir['graylog_illuminate_core/*'],
+                 Dir['graylog_illuminate_windows/*'],
+                 Dir['graylog_illuminate_okta/*'],
+                 Dir['graylog_illuminate_paloalto/*'],
+                 Dir['graylog_illuminate_o365/*']
 
   fpm_attributes rpm_os: 'linux'
+
+  post_install   'files/post-install'
+  post_uninstall 'files/post-uninstall'
 
   def build
     system("tar -xvzf graylog_illuminate_server_files*.tar.gz")
   end
 
   def install
-    etc('graylog/illuminate').install 'README.txt'
-    etc('graylog/illuminate').install Dir['graylog_illuminate_*/', 'illuminate_elastic_template']
+    etc('graylog/graylog_illuminate_core').install Dir['graylog_illuminate_core/*']
+    etc('graylog/graylog_illuminate_core').chmod(0755)
+
+    etc('graylog/graylog_illuminate_windows').install Dir['graylog_illuminate_windows/*']
+    etc('graylog/graylog_illuminate_windows').chmod(0755)
+
+    etc('graylog/graylog_illuminate_okta').install Dir['graylog_illuminate_okta/*']
+    etc('graylog/graylog_illuminate_okta').chmod(0755)
+
+    etc('graylog/graylog_illuminate_paloalto').install Dir['graylog_illuminate_paloalto/*']
+    etc('graylog/graylog_illuminate_paloalto').chmod(0755)
+
+    etc('graylog/graylog_illuminate_o365').install Dir['graylog_illuminate_o365/*']
+    etc('graylog/graylog_illuminate_o365').chmod(0755)
   end
 end
