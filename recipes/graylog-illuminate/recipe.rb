c5637c0788ea71c0e7d5ef8733073d2e12f2b59e
Donald Morton <donald.morton@graylog.com>
2021-03-24 12:42:07 -0500

    Create recipe for Graylog Illuminate (#104)
    * Create Illuminate package.
    
    Co-authored-by: Bernd Ahlers <bernd@users.noreply.github.com>

---
+++ recipes/graylog-illuminate/recipe.rb
@@ -0,0 +1,58 @@
+require_relative '../tools'
+
+class GraylogIlluminate < FPM::Cookery::Recipe
+  description 'Graylog Illuminate'
+
+  name     'graylog-illuminate'
+  version  data.version
+  revision data.revision
+  homepage 'https://graylog.org'
+  arch     'amd64'
+
+  source data.source
+  sha256 data.sha256
+
+  maintainer 'Graylog, Inc. <hello@graylog.org>'
+  vendor     'graylog'
+  license    data.license
+
+  config_files [
+    'graylog_illuminate_core',
+    'graylog_illuminate_windows',
+    'graylog_illuminate_okta',
+    'graylog_illuminate_paloalto',
+    'graylog_illuminate_o365',
+    'illuminate_elastic_template'
+  ].map {|type|
+    Dir["#{type}/*"].map {|f|
+      File.join("/etc/graylog/#{type}", File.basename(f))
+    }
+  }.flatten
+
+  post_install   'files/post-install'
+  post_uninstall 'files/post-uninstall'
+
+  def build
+    system("tar -xvzf graylog_illuminate_server_files*.tar.gz")
+  end
+
+  def install
+    etc('graylog/graylog_illuminate_core').install Dir['graylog_illuminate_core/*']
+    etc('graylog/graylog_illuminate_core').chmod(0755)
+
+    etc('graylog/graylog_illuminate_windows').install Dir['graylog_illuminate_windows/*']
+    etc('graylog/graylog_illuminate_windows').chmod(0755)
+
+    etc('graylog/graylog_illuminate_okta').install Dir['graylog_illuminate_okta/*']
+    etc('graylog/graylog_illuminate_okta').chmod(0755)
+
+    etc('graylog/graylog_illuminate_paloalto').install Dir['graylog_illuminate_paloalto/*']
+    etc('graylog/graylog_illuminate_paloalto').chmod(0755)
+
+    etc('graylog/graylog_illuminate_o365').install Dir['graylog_illuminate_o365/*']
+    etc('graylog/graylog_illuminate_o365').chmod(0755)
+
+    etc('graylog/illuminate_elastic_template').install Dir['illuminate_elastic_template/*']
+    etc('graylog/illuminate_elastic_template').chmod(0755)
+  end
+end
