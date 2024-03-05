f126aad10a5128ef09f3e187e957970445b35f34
Bernd Ahlers <bernd@graylog.com>
2024-03-05 14:58:55 +0100

    Adjust update_sha.py to automatically update revision and other values
    (cherry picked from commit dbb5e5f441609f1dfc78d8cf797e6540bc0dd14d)
    (cherry picked from commit 2ab7d41de6df6fc638c8c5bd5d6dfda508faf963)

--- scripts/update_sha.py
+++ scripts/update_sha.py
@@ -12,26 +12,28 @@ class MyDumper(yaml.SafeDumper):
         if len(self.indents) == 1:
             super().write_line_break()
 
-yaml_comments = """  # Make sure to always increase the revision when doing alpha/beta/rc releases!
-  # Example:
+yaml_comments = """  # The "revision" must always be bumped as long as the version doesn't change!
+  # This is important to make updates in pre-releases work correctly with
+  # apt/deb and yum/dnf/rpm.
   #
-  #   - 2.1.0-beta.1  => version=2.1.0, revision="1.beta.1"
-  #   - 2.1.0-beta.2  => version=2.1.0, revision="2.beta.2"
-  #   - 2.1.0-rc.1    => version=2.1.0, revision="3.rc.1"
-  #   - 2.1.0         => version=2.1.0, revision="4"
+  #   - 2.1.0-alpha.1 => version=2.1.0, revision="1.alpha.1"
+  #   - 2.1.0-alpha.2 => version=2.1.0, revision="2.alpha.2"
+  #   - 2.1.0-beta.1  => version=2.1.0, revision="3.beta.1"
+  #   - 2.1.0-beta.2  => version=2.1.0, revision="4.beta.2"
+  #   - 2.1.0-rc.1    => version=2.1.0, revision="5.rc.1"
+  #   - 2.1.0         => version=2.1.0, revision="6"
+  #   - 2.1.1         => version=2.1.1, revision="1"
   #   - 2.2.0-alpha.1 => version=2.2.0, revision="1.alpha.1"
+  #   - 2.2.0-beta.1  => version=2.2.0, revision="2.beta.1"
   #
-  # Only reset the revision once the version is bumped."""
+  # Only set the "revision" back to "1" when the version changes!"""
 
 parser = argparse.ArgumentParser(description='Update values in data.yml')
 parser.add_argument('--path', dest='yaml_path', help='The path to the yaml file to update.', required=True)
 parser.add_argument('--package-name', dest='package_name', help='The name of the package that should be updated.')
 parser.add_argument('--arch', dest='arch', default="all", help='The architecture of the package that should be updated.')
 parser.add_argument('--sha256', dest='checksum', help="The sha256 value that should be set for the package.")
-parser.add_argument('--version-major', dest='version_major', help='The major version (e.g, 3.3, 4.0)')
 parser.add_argument('--version', dest='version', help='The semantic version (3.3.0, 4.0.0)')
-parser.add_argument('--suffix', dest='suffix', help='The package suffix (e.g, -rc.1). Must use equal sign if suffix begins with dash. (--suffix=-rc.2). To blank this value, pass --suffix without any argument after it.', nargs='?', const='NO_VALUE')
-parser.add_argument('--revision', dest='revision', help='The package revision. Subsequent releases for the same version must bump this. It only resets when the version goes up.')
 
 args = parser.parse_args()
 
@@ -52,20 +54,60 @@ with open(args.yaml_path) as f:
                 raise RuntimeError(f'Missing {source_field} field in {args.yaml_path} for package {args.package_name}')
             data[args.package_name][f'sha256_{args.arch}'] = args.checksum
 
-    if args.version_major:
-        data['default']['version_major'] = args.version_major
-
     if args.version:
-        data['default']['version'] = args.version
+        # The "revision" must always be bumped as long as the major.minor.patch
+        # version doesn't change! This is important to make updates in pre-releases
+        # work correctly with apt/deb and yum/dnf/rpm.
+        #
+        #   - 2.1.0-alpha.1 => version=2.1.0, revision="1.alpha.1"
+        #   - 2.1.0-alpha.2 => version=2.1.0, revision="2.alpha.2"
+        #   - 2.1.0-beta.1  => version=2.1.0, revision="3.beta.1"
+        #   - 2.1.0-beta.2  => version=2.1.0, revision="4.beta.2"
+        #   - 2.1.0-rc.1    => version=2.1.0, revision="5.rc.1"
+        #   - 2.1.0         => version=2.1.0, revision="6"
+        #   - 2.1.1         => version=2.1.1, revision="1"
+        #   - 2.2.0-alpha.1 => version=2.2.0, revision="1.alpha.1"
+        #   - 2.2.0-beta.1  => version=2.2.0, revision="2.beta.1"
+        #
+        # Only set the "revision" back to "1" when the major.minor.patch version
+        # changes!
 
-    if args.suffix:
-        if args.suffix == 'NO_VALUE':
-            data['default']['suffix'] = ''
+        # 6.0.0-alpha.1 => version="6.0.0", suffix=["alpha.1"]
+        # 6.0.0         => version="6.0.0", suffix=[]
+        version, *suffixes = args.version.split('-', 1)
+        # 6.0.0 => major=6, minor=0, patch= 0
+        major, minor, patch = version.split('.', 2)
+
+        suffix = suffixes[0] if len(suffixes) > 0 else None
+
+        if data['default']['version'] == version:
+            # The version didn't change, so we have to handle the revision.
+            # "1.alpha.1" => rev_str="1" *_=["alpha.1"]
+            rev_str, *_ = data['default']['revision'].split('.', 1)
+
+            if not suffix and data['default']['suffix']:
+                # Bump the revision when there was a suffix set but the new
+                # version doesn't have a suffix. (1.0.0-rc.1 => 1.0.0)
+                data['default']['revision'] = str(int(rev_str) + 1)
+            elif suffix and suffix != data['default']['suffix'].lstrip('-'):
+                # Bump the revision when the suffix changes. (1.0.0-alpha.1 => 1.0.0-alpha.2)
+                data['default']['revision'] = str(int(rev_str) + 1) + '.' + suffix
+            else:
+                # Keep the revision number because the version and suffix
+                # doesn't change. (1.0.0-rc.1 => 1.0.0-rc.1)
+                pass
         else:
-            data['default']['suffix'] = args.suffix
+            # Reset revision to "1" when the major.minor.patch version changes
+            data['default']['revision'] = '1'
 
-    if args.revision:
-        data['default']['revision'] = args.revision
+        data['default']['version'] = version
+        data['default']['version_major'] = '.'.join([major, minor])
+
+        # The suffix only exists for pre-releases
+        if suffix:
+            data['default']['suffix'] = '-' + suffix
+        else:
+            data['default']['suffix'] = ''
 
 #write out the new yaml file
 with open(args.yaml_path, 'w') as f:
